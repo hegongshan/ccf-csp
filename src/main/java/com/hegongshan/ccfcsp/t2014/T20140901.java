10d78bf69219e80e3da5d619f1bdaae2fd8d8afe
hegongshan <hegongshan@qq.com>
2018-08-02 14:12:44 +0800

    add 201403-1, 201409-1, 201509-1, 201512-1

---
+++ src/main/java/com/hegongshan/ccfcsp/t2014/T20140901.java
@@ -0,0 +1,32 @@
+package com.hegongshan.ccfcsp.t2014;
+
+import java.util.Scanner;
+
+/**
+ * 相邻数对
+ * @author hegongshan https://www.hegongshan.com
+ *
+ */
+public class T20140901 {
+	public static void main(String[] args) {
+		// 1. 获得输入数据
+		Scanner scan = new Scanner(System.in);
+		int total = scan.nextInt();
+		int[] arr = new int[total];
+		for (int i = 0; i < arr.length ; i++) {
+			arr[i] = scan.nextInt();
+		}
+		scan.close();
+		
+		//　2. 遍历数组arr，记录相差1的数对的个数
+		int count = 0;
+		for (int i = 0; i < arr.length; i++) {
+			for (int j = i + 1; j < arr.length; j++) {
+				if(Math.abs(arr[i]-arr[j]) == 1) {
+					count ++;
+				}
+			}
+		}
+		System.out.println(count);
+	}
+}
---
+++ src/main/java/com/hegongshan/ccfcsp/t2015/T20150901.java
@@ -0,0 +1,26 @@
+package com.hegongshan.ccfcsp.t2015;
+
+import java.util.Scanner;
+
+/**
+ * 数列分段
+ * @author hegongshan https://www.hegongshan.com
+ *
+ */
+public class T20150901 {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int total = scan.nextInt();
+		int[] arr = new int[total];
+		int count = total;
+		for (int i = 0; i < arr.length ; i++) {
+			arr[i] = scan.nextInt();
+			if(i > 0 && arr[i] == arr[i-1]) {
+				count--;
+			}
+		}
+		scan.close();
+		System.out.println(count);
+		
+	}
+}
