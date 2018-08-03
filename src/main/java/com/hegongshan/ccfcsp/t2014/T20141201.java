c277b0229daa81c832ff0a505a567e4c543ba8b8
hegongshan <hegongshan@qq.com>
2018-08-03 13:32:53 +0800

    add 201412-1 and 201503-1

---
+++ src/main/java/com/hegongshan/ccfcsp/t2014/T20141201.java
@@ -0,0 +1,31 @@
+package com.hegongshan.ccfcsp.t2014;
+
+import java.util.Scanner;
+
+/**
+ * 门禁系统
+ * 
+ * @author hegongshan https://www.hegongshan.com
+ *
+ */
+public class T20141201 {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int total = scan.nextInt();
+		int[][] arr = new int[total][2];
+		for (int i = 0; i < arr.length; i++) {
+			arr[i][0] = scan.nextInt();
+		}
+		scan.close();
+
+		for (int i = 0; i < arr.length; i++) {
+			for (int j = i; j >= 0; j--) {
+				if (arr[i][0] == arr[j][0]) {
+					arr[i][1]++;
+				}
+			}
+			System.out.print(arr[i][1] + " ");
+		}
+
+	}
+}
---
+++ src/main/java/com/hegongshan/ccfcsp/t2015/T20150301.java
@@ -0,0 +1,34 @@
+package com.hegongshan.ccfcsp.t2015;
+
+import java.util.Scanner;
+
+/**
+ * 图像旋转
+ * @author hegongshan
+ *
+ */
+public class T20150301 {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int row = scan.nextInt();
+		int column = Integer.parseInt(scan.nextLine().trim());
+		int[][] arr = new int[row][column];
+		for (int i = 0; i < arr.length; i++) {
+			String[] sArr = scan.nextLine().split(" ");
+			for (int j = 0; j < arr[i].length; j++) {
+				arr[i][j] = Integer.parseInt(sArr[j]);
+			}
+		}
+		scan.close();
+		
+		//原来的行变为现在的列，且元素倒排
+		int index = column;
+		for (int i = 0; i < column; i++) {
+			for (int j = 0; j < row; j++) {
+				System.out.print(arr[j][index-1]+" ");
+			}
+			index--;
+			System.out.println();
+		}
+	}
+}
