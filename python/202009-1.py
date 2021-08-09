dba359f48b76f1f34935a497738c48eaf0b25ec7
hegongshan <hegongshan@qq.com>
2021-08-10 05:42:26 +0800

    optimize:update 202009-1.py

--- python/202009-1.py
+++ python/202009-1.py
@@ -2,11 +2,11 @@ def main():
 	n, x, y = map(int, input().split(' '))
 	arr = []
 	for i in range(1, n + 1):
-		loc = list(map(int, input().split(' ')))
-		dist = (loc[0] - x) ** 2 + (loc[1] - y) ** 2
+		xi, yi = list(map(int, input().split(' ')))
+		dist = (xi - x) ** 2 + (yi - y) ** 2
 		arr.append([i, dist])
 
-	# 首先按距离升学排序，当距离相等时，按照监测点编号升序排序
+	# 首先按距离升序排序，当距离相等时，按照检测点编号升序排序
 	res = sorted(arr, key=lambda x: (x[1], x[0]))[:3]
 	for i in range(3):
 		print(res[i][0])
